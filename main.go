28c309f458af64d178c40c9baa8740cc30adac03
王守虎 <sszu@qq.com>
2018-12-26 20:02:27 +0800

    http服务

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "hello world")
+}
+
+func main() {
+	http.HandleFunc("/", IndexHandler)
+	fmt.Println("开启服务")
+	http.ListenAndServe(":8000", nil)
+}
